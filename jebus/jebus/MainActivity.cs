b3c3ee29e5196b720e3a626bde053324522dfe30
Kaguedes <tiagoasc.69@hotmail.com>
2014-07-17 02:11:54 +0100

    bla bla
    core

---
+++ jebus/jebus/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace jebus
+{
+	[Activity (Label = "jebus", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+			button.Click += delegate {
+				String input = FindViewById<AutoCompleteTextView> (Resource.Id.inputText).Text;
+				String output = input.Replace("hi","Yo! sup");
+				output = output.Replace("man", "Swagger");
+				output = output.Replace("girl", "bitch");
+				output = output.Replace("girl", "bitch");
+				output = output.Replace("cool", "swaggy");
+				output = output.Replace("dude", "nigger");
+				FindViewById<EditText> (Resource.Id.outputText).Text = output; 
+			};
+		}
+	}
+}
+
