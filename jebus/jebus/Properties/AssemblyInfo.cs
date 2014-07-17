6890383e547b918e2c25d29cef996b4c01124556
Badim150 <cm_margarido@hotmail.com>
2014-07-17 05:04:23 +0100

    final 1.0

--- jebus/jebus/Properties/AssemblyInfo.cs
+++ jebus/jebus/Properties/AssemblyInfo.cs
@@ -16,6 +16,7 @@ using Android.App;
 [assembly: Application(Icon = "@drawable/icon")]
 
 
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
